73fbcb6a79ee50abd6f25843ec972ac850ddf1d4
vatsalrathod07 <vatsalrathod2005@gmail.com>
2025-02-09 14:54:47 +0530

    Make It Responsive

--- Portfolio/script.js
+++
@@ -1,9 +0,0 @@
-let index = 0;
-const slides = document.querySelector("#carousel");
-
-function nextSlide() {
-    index = (index + 1) % 3; // Change '3' to the total number of slides
-    slides.style.transform = `translateX(-${index * 100}%)`;
-}
-
-setInterval(nextSlide, 300);
